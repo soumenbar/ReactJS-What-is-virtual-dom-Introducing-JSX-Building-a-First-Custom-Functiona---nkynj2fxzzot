0e4d902711e230f7abb670b138f4cd3169c8a3be
soumenbar <barsoumen33@gmail.com>
2022-07-20 09:52:41 +0530

    timer assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,37 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  let[timee, setTimee]=useState("");
+
+  let num;
+  let intVal;
+  
+  
+  function countDown(e){
+    if(e.keyCode==13){
+      clearInterval(intVal);
+      num=document.getElementById("timeCount").value;
+      
+      setTimee(num--);
+      intVal=setInterval(()=>{
+        setTimee(num--);
+        if(num<0)
+          clearInterval(intVal);
+      },1000);
+    }
+  }
 
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={(e)=>{
+            countDown(e);
+
+          }}/> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{timee}</div>
     </div>
   )
 }
